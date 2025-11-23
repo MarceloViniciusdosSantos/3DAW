28237a8c4da0d0e553ce4c7a79d47e303501d00e
Marcelo Vinícius <142310322+MarceloViniciusdosSantos@users.noreply.github.com>
2025-11-23 11:29:13 -0300

    Delete ViacaoCalango/viacao_calango directory
    Upei errado :P

--- ViacaoCalango/viacao_calango/public/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="site-footer">
-  <div>Contato: viacaoclaro@email.com | (21)9000-0000</div>
-  <div>&copy; 2025 Viação</div>
-</footer>
-<script src="<?php echo $assets_path; ?>/assets/js/app.js"></script>
-</body>
-</html>
