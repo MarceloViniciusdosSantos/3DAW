034f1d44928bf8bed2df31f553f9a7a815279352
Marcelo Vinícius <142310322+MarceloViniciusdosSantos@users.noreply.github.com>
2025-11-23 11:31:04 -0300

    Delete viacao_calango directory

--- viacao_calango/public/views/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="site-footer">
-  <div>Contato: viacaocalango@email.com | (21)98886-5973</div>
-  <div>&copy; 2025 ViaçãoCalango</div>
-</footer>
-<script src="<?php echo $assets_path; ?>/assets/js/app.js"></script>
-</body>
-</html>
--- viacao_calango/public/views/sobre.php
+++
@@ -1,7 +0,0 @@
-<div class="box">
-  <h2>Sobre a Viação</h2>
-  <p>A Viação Calango é uma renomada empresa de transporte interestadual que tem como 
-    objetivo proporcionar aos seus passageiros uma experiência de viagem segura,
-    confortável e confiável. Com uma trajetória sólida e uma equipe dedicada,
-    somos referência no setor e nos orgulhamos de oferecer serviços da mais alta qualidade.</p>
-</div>
